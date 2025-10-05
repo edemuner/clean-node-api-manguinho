db6afb226be46fdf0595d41d88549c92c8ba93ba
edemuner <eduardood15@hotmail.com>
2025-10-05 15:04:20 -0300

    chore: add eslint (tentative)

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+    tseslint.configs.strict,
+    tseslint.configs.stylistic,
+);
