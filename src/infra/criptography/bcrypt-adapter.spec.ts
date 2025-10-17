fdffe361224ff17e4ec27c738cd4457b761b0b02
eduardo <eduardo.oliveira@lumahealth.io>
2025-10-17 20:20:46 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -25,6 +25,14 @@ describe('Bcrypt Adapter', () => {
         const sut = makeSut();
         const hash = await sut.encrypt('any_value');
         expect(hash).toBe('hash');
-    })
+    });
+
+    test('Should throw if bcrypt throws', async () => {
+        const sut = makeSut();
+        (jest.spyOn(bcrypt, 'hash') as jest.Mock).mockRejectedValueOnce(new Error());
+        const promise = sut.encrypt('any_value');
+        await expect(promise).rejects.toThrow();
+    });
+
 
 })
