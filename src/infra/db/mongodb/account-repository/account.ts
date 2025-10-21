b1378c952ff39a5b3c10354598584da2733b403d
eduardo <eduardo.oliveira@lumahealth.io>
2025-10-20 21:30:18 -0300

    refactor: move mapping to mongo helper

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -9,9 +9,11 @@ export class AccountMongoRepository implements AddAccountRepository {
     async add(accountData: AddAccountModel): Promise<AccountModel> {
         const accountCollection = MongoHelper.getCollection('accounts');
         const result = await accountCollection.insertOne(accountData);
-        return {
-            id: result.insertedId.toString(),
-            ...accountData
-        };
+        if (result.insertedId){
+            return MongoHelper.map({
+                _id: result.insertedId.toString(),
+                ...accountData
+            });
+        }
     }
 }
