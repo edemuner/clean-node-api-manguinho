0a5e139dffbda96986bd9c7e1847240d5dfd11ae
eduardo <eduardo.oliveira@lumahealth.io>
2025-10-22 20:50:01 -0300

    feat: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,11 @@
-import app from "./config/app";
+import { MongoHelper } from "../infra/db/mongodb/helpers/mongo-helper";
+import env from "./config/env";
 
-app.listen(5051, () => {
-    console.log('Server running at http://localhost:5051');
-});
+MongoHelper.connect(env.mongoUrl)
+    .then(async () => {
+        const app = (await import("./config/app")).default;
+        app.listen(env.port, () => {
+            console.log(`Server running at http://localhost:${env.port}`);
+        });
+    })
+    .catch(console.error);
