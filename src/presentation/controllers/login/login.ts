6256a416f4668894bcb9d332808015b72bd27f2c
eduardo <eduardo.oliveira@lumahealth.io>
2025-11-12 19:59:41 -0300

    feat: ensure LoginController calls emailValidator.isvalid with correct email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,9 +1,16 @@
 import { Controller, HttpRequest, HttpResponse } from '../../protocols';
 import { badRequest } from '../../helpers/http-helper';
 import { MissingParamError } from '../../errors';
+import { EmailValidator } from '../signup/signup-protocols';
 
 
 export class LoginController implements Controller {
+
+    private readonly emailValidator;
+
+    constructor(emailValidator: EmailValidator){
+        this.emailValidator = emailValidator;
+    }
 
     async handle(httpRequest: HttpRequest): Promise<HttpResponse>{
         if (!httpRequest.body.password){
@@ -13,5 +20,7 @@ export class LoginController implements Controller {
         if (!httpRequest.body.email){
             return badRequest(new MissingParamError('email'));
         }
+
+        this.emailValidator.isValid(httpRequest.body.email);
     }
 }
