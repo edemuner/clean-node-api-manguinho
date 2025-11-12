725ca89a2539654ac354b6d2434562a558017863
eduardo <eduardo.oliveira@lumahealth.io>
2025-11-12 19:48:13 -0300

    test: ensure loginController returns 400 if no password is provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -12,5 +12,17 @@ describe('Login controller',  () => {
         }
         const httpResponse = await sut.handle(httpRequest);
         expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
-    })
+    });
+
+    test('Shold return 400 if no password is provided', async () => {
+        const sut = new LoginController();
+        const httpRequest = {
+            body: {
+                email: 'any_email@email.com'
+            }
+        }
+        const httpResponse = await sut.handle(httpRequest);
+        expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+    });
+
 })
