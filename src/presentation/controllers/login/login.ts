e5ba92ec32d686d2a1ed620ad0265e97b3342c6e
eduardo <eduardo.oliveira@lumahealth.io>
2025-11-12 20:03:30 -0300

    feat: ensure LoginController returns 400 if an invalid email is provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,6 +1,6 @@
 import { Controller, HttpRequest, HttpResponse } from '../../protocols';
 import { badRequest } from '../../helpers/http-helper';
-import { MissingParamError } from '../../errors';
+import { InvalidParamError, MissingParamError } from '../../errors';
 import { EmailValidator } from '../signup/signup-protocols';
 
 
@@ -21,6 +21,9 @@ export class LoginController implements Controller {
             return badRequest(new MissingParamError('email'));
         }
 
-        this.emailValidator.isValid(httpRequest.body.email);
+        const isValid = this.emailValidator.isValid(httpRequest.body.email);
+        if (!isValid){
+            return badRequest(new InvalidParamError('email'));
+        }
     }
 }
