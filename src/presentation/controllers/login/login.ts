9466e113beca471586593d7ae8092cab85e5e68d
eduardo <eduardo.oliveira@lumahealth.io>
2025-11-13 20:47:32 -0300

    feat: ensure LoginController returns 200 when authentication succeeeds

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { Controller, HttpRequest, HttpResponse, EmailValidator, Authentication } from './login-protocols';
-import { badRequest, serverError, unauthorized } from '../../helpers/http-helper';
+import { badRequest, ok, serverError, unauthorized } from '../../helpers/http-helper';
 import { InvalidParamError, MissingParamError } from '../../errors';
 
 
@@ -35,6 +35,10 @@ export class LoginController implements Controller {
             if(!accessToken){
                 return unauthorized();
             }
+
+            return ok({
+                accessToken
+            })
         } catch(error){
             return serverError(error);
         }
