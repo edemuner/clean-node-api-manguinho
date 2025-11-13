c8ffe57948983f2f747a21628531a9792207dcb4
eduardo <eduardo.oliveira@lumahealth.io>
2025-11-13 20:08:22 -0300

    test: ensure LoginContorller calls authentication with correct values

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -2,14 +2,17 @@ import { Controller, HttpRequest, HttpResponse } from '../../protocols';
 import { badRequest, serverError } from '../../helpers/http-helper';
 import { InvalidParamError, MissingParamError } from '../../errors';
 import { EmailValidator } from '../signup/signup-protocols';
+import { Authentication } from '../../../domain/usecases/authentication';
 
 
 export class LoginController implements Controller {
 
     private readonly emailValidator;
+    private readonly authentication;
 
-    constructor(emailValidator: EmailValidator){
+    constructor(emailValidator: EmailValidator, authentication: Authentication){
         this.emailValidator = emailValidator;
+        this.authentication =  authentication;
     }
 
     async handle(httpRequest: HttpRequest): Promise<HttpResponse>{
@@ -28,7 +31,9 @@ export class LoginController implements Controller {
             const isValid = this.emailValidator.isValid(email);
             if (!isValid){
                 return badRequest(new InvalidParamError('email'));
-            }    
+            }
+
+            await this.authentication.auth(email, password);
         } catch(error){
             return serverError(error);
         }
