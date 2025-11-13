1c0fc1e6468b30e062db5c4eefb3b26e53ed5bec
eduardo <eduardo.oliveira@lumahealth.io>
2025-11-13 19:53:41 -0300

    test: ensure LoginContorller returns 500 if emailValidator throws

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,5 +1,5 @@
 import { LoginController } from "./login";
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, serverError } from '../../helpers/http-helper'
 import { InvalidParamError, MissingParamError } from "../../errors";
 import { EmailValidator, HttpRequest } from "../signup/signup-protocols";
 
@@ -71,5 +71,15 @@ describe('Login controller',  () => {
         const httpResponse = await sut.handle(makeFakeRequest());
         expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')));
     });
+
+    test('Shold return 500 if email validator throws', async () => {
+        const { sut, emailValidatorStub } = makeSut();
+        jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+            throw new Error();
+        });
+        const httpResponse = await sut.handle(makeFakeRequest());
+        expect(httpResponse).toEqual(serverError(new Error()));
+    });
+
 
 })
