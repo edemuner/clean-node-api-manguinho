6a91b7dbca9aac4b74ecedc6fba6f717a330e9eb
eduardo <eduardo.oliveira@lumahealth.io>
2025-11-13 20:35:51 -0300

    test: ensure LoginController returns 401 if invalid credentials are provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,5 +1,5 @@
 import { LoginController } from "./login";
-import { badRequest, serverError } from '../../helpers/http-helper'
+import { badRequest, serverError, unauthorized } from '../../helpers/http-helper'
 import { InvalidParamError, MissingParamError } from "../../errors";
 import { EmailValidator, HttpRequest } from "../signup/signup-protocols";
 import { Authentication } from "../../../domain/usecases/authentication";
@@ -103,4 +103,10 @@ describe('Login controller',  () => {
         expect(authSpy).toHaveBeenCalledWith('any_email@email.com', 'any_password');
     });
 
+    test('Shold return 401 if invalid credentials are provided', async () => {
+        const { sut, authenticationStub } = makeSut();
+        jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(new Promise(resolve => resolve(null)));
+        const httpResponse = await sut.handle(makeFakeRequest());
+        expect(httpResponse).toEqual(unauthorized());
+    });
 })
