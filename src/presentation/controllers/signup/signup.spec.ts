73fd2bd1f5a9c8522c79f4f2edaa8ffbd9a174ea
eduardo <eduardo.oliveira@lumahealth.io>
2025-11-17 20:37:29 -0300

    test: ensure SignupController returns 400 if validation throws error

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -214,4 +214,13 @@ describe('Signup Controller', () => {
         expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
     })
 
+    test('should return 400 if validation returns an error', async () => {
+        const { sut, validationStub } = makeSut();
+        jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'));
+        const httpRequest = makeFakeRequest();
+        const httpResponse = await sut.handle(httpRequest);
+        expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')));
+    })
+
+
 });
