958aeebb6f13d66184b26bf8f83718015ae9368d
eduardo <eduardo.oliveira@lumahealth.io>
2025-11-18 18:49:14 -0300

    refactor: move password comparison to validationComposite

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -79,21 +79,6 @@ const makeFakeAccount = (): AccountModel => {
 
 describe('Signup Controller', () => {
 
-    test('should return 400 if password confirmation fails', async () => {
-        const { sut } = makeSut();
-        const httpRequest = {
-            body: {
-                name: 'any_name',
-                email: 'any_email@email.com',
-                password: 'any_password',
-                passwordConfirmation: 'diff_password'
-            }
-        }
-        const httpResponse = await sut.handle(httpRequest);
-        expect(httpResponse).toEqual(badRequest(new InvalidParamError('passwordConfirmation')))
-
-    })
-
     test('should return 400 if an invalid email is provided', async () => {
         const { sut, emailValidatorStub } = makeSut();
         jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false);
