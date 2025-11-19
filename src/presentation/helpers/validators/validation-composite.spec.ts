3d1c37f9845a61d551d398c43601d4f695741bf5
eduardo <eduardo.oliveira@lumahealth.io>
2025-11-19 20:31:05 -0300

    refactor: move sut creation to a factory method

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -4,18 +4,31 @@ import { ValidationComposite } from "./validation-composite";
 
 describe('Validation composite', () => {
 
-    const makeSut = (validationStub): ValidationComposite => {
-        return new ValidationComposite([validationStub]);
+    interface SutTypes {
+        sut: ValidationComposite,
+        validationStub: Validation
+    }
+
+    const makeValidation = (): Validation => {
+        class ValidationStub implements Validation {
+            validate(input: any): Error {
+                return null;
+            }
+        }
+        return new ValidationStub();
+    }
+
+    const makeSut = (): SutTypes => {
+        const validationStub = makeValidation();
+        return {
+            sut: new ValidationComposite([validationStub]),
+            validationStub
+        }
     }
 
     test('Should return an error if any validation fails', () => {
-        class ValidationStub implements Validation {
-            validate(input: any): Error {
-                return new MissingParamError('field');
-            }
-        }
-        const validationStub = new ValidationStub();
-        const sut = makeSut(validationStub);
+        const { sut, validationStub } = makeSut();
+        jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('field'))
         const error = sut.validate({ field: 'any_value' });
         expect(error).toEqual(new MissingParamError('field'));
     });
