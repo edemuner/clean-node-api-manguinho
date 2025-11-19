749d9ad829d92f8cbea28cf4303ef9a680877d71
eduardo <eduardo.oliveira@lumahealth.io>
2025-11-19 19:49:22 -0300

    refactor: move sut creation to a factory method

--- src/presentation/helpers/validators/required-fields-validation.spec.ts
+++ src/presentation/helpers/validators/required-fields-validation.spec.ts
@@ -3,14 +3,18 @@ import { RequiredFieldValidation } from "./required-field-validation";
 
 describe('RequiredFields Validation', () => {
 
+    const makeSut = (): RequiredFieldValidation => {
+        return new RequiredFieldValidation('any_field');
+    }
+
     test('Should return a MissingParamError if validation fails', () => {
-        const sut = new RequiredFieldValidation('any_field');
+        const sut = makeSut();
         const error = sut.validate({ name: 'any_name'});
         expect(error).toEqual(new MissingParamError('any_field'));
     });
 
     test('Should not return if validation succeeds', () => {
-        const sut = new RequiredFieldValidation('any_field');
+        const sut = makeSut();
         const error = sut.validate({ any_field: 'content'});
         expect(error).toBeFalsy();
     });
