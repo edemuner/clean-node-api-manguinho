ab9dbf1f9651213a47c8480c8db2205f1fd9b1e3
eduardo <eduardo.oliveira@lumahealth.io>
2025-11-19 19:48:16 -0300

    test: ensure RequiredFieldValidation returns falsy if validation succeeds

--- src/presentation/helpers/validators/required-fields-validation.spec.ts
+++ src/presentation/helpers/validators/required-fields-validation.spec.ts
@@ -2,9 +2,17 @@ import { MissingParamError } from "../../errors";
 import { RequiredFieldValidation } from "./required-field-validation";
 
 describe('RequiredFields Validation', () => {
+
     test('Should return a MissingParamError if validation fails', () => {
         const sut = new RequiredFieldValidation('any_field');
         const error = sut.validate({ name: 'any_name'});
         expect(error).toEqual(new MissingParamError('any_field'));
     });
+
+    test('Should not return if validation succeeds', () => {
+        const sut = new RequiredFieldValidation('any_field');
+        const error = sut.validate({ any_field: 'content'});
+        expect(error).toBeFalsy();
+    });
+
 });
