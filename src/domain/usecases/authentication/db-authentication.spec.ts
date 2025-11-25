983d61485479f2055d1c62bbb2d195edfea9c81b
eduardo <eduardo.oliveira@lumahealth.io>
2025-11-24 22:06:58 -0300

    test: ensure DbAuthentication throws if LoadAccountByEmailRepository throws

--- src/domain/usecases/authentication/db-authentication.spec.ts
+++ src/domain/usecases/authentication/db-authentication.spec.ts
@@ -51,4 +51,12 @@ describe('DbAuthentication Usecase', () => {
         await sut.auth(makeFakeAuthentication());
         expect(loadSpy).toHaveBeenCalledWith('any_email@email.com');
     });
+
+    test('Should throw if LoadAccountByEmailRepository throws', async () => {
+        const { sut, loadAccountByEmailRepositoryStub } = makeSut();
+        jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())));
+        const promise = sut.auth(makeFakeAuthentication());
+        expect(promise).rejects.toThrow();
+    });
+
 });
