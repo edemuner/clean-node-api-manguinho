367cbce0fc9f7e18adabc760914d9fe485967ae5
eduardo <eduardo.oliveira@lumahealth.io>
2025-11-25 20:48:28 -0300

    test: ensure dbAuthentication returns null if HashComparer returns false

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -93,4 +93,11 @@ describe('DbAuthentication Usecase', () => {
         expect(promise).rejects.toThrow();
     });
 
+    test('Should return null if HashComparer returns false', async () => {
+        const { sut, hashComparerStub } = makeSut();
+        jest.spyOn(hashComparerStub, 'compare').mockReturnValueOnce(new Promise(resolve => resolve(false)));
+        const accessToken = await sut.auth(makeFakeAuthentication());
+        expect(accessToken).toBe(null);
+    });
+
 });
