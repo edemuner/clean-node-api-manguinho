92982090293e62dc732d5c70bf7f464ef97113a7
eduardo <eduardo.oliveira@lumahealth.io>
2025-11-25 21:28:54 -0300

    test: ensure DbAuthentication throws if UpdateAccessTokenRepository throws

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -155,4 +155,11 @@ describe('DbAuthentication Usecase', () => {
         expect(updateSpy).toHaveBeenCalledWith('any_id', 'any_token');
     });
 
+    test('Should throw if UpdateAccessTokenRepository throws', async () => {
+        const { sut, updateAccessTokenRepositoryStub } = makeSut();
+        jest.spyOn(updateAccessTokenRepositoryStub, 'update').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())));
+        const promise = sut.auth(makeFakeAuthentication());
+        expect(promise).rejects.toThrow();
+    });
+
 });
